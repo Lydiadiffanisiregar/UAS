de82c1d4c30b877ac7df69173d8de1d91bbca53a
Lydiadiffanisiregar <lydiadiffanii@gmail.com>
2022-07-14 10:36:31 +0700

    foto

---
+++ app/Views/index.php
@@ -0,0 +1,12 @@
+<?= $this->extend('/layouts/tempelate'); ?>
+
+<?= $this->section('content'); ?>
+<div class="container">
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQn2fooM3tyt2t_WmQ3r42h4JMHxZWCLT2NDw&usqp=CAU" height="180px"
+align="right" border="5">
+
+    <h1>SELAMAT DATANG DI COFFEE SHOP!</h1>
+    <p>Silahkan mengambil antrian</p>
+
+</div>
+<?= $this->endSection(); ?>
